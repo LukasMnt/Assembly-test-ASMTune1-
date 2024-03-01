045aa7c02e33c7d56dbcf93678624c4fff0b0b40
Lukas <moilukasdu85@gmail.com>
2024-03-01 16:08:29 +0100

    32 which erase is avaliable for every memory unit so careful, ah and  register is way better than watch (espion)

--- ASMTune1/main.asm
+++ ASMTune1/main.asm
@@ -1,18 +1,8 @@
 .code
 SomeFunction proc
 
-	mov r8, -1
-	mov r8d, 0 ; 32 bits pas compatible 64
-	mov r8w, -1
-	mov r8b, 0
-
-
-	push rbx
-
-	mov bx, 50
-	mov bh, 10
-
-	pop rbx
+	mov r11, -1
+	mov r11d, -1
 
 	ret
 SomeFunction endp
