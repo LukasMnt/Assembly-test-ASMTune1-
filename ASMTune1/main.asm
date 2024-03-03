93cde2ffd2da0c0a87011684484e678108d83da4
Lukas <moilukasdu85@gmail.com>
2024-03-03 20:08:01 +0100

    dtypes

--- ASMTune1/main.asm
+++ ASMTune1/main.asm
@@ -1,9 +1,14 @@
+.data
+myByte db 78
+myWord dw ?
+myFloat real4 95.3
+
+
 .code
 SomeFunction proc
-
-	mov r11, -1
-	mov r11d, -1
-
+	mov al, byte ptr [myByte] ; or just mov al, myByte
+	mov ebx, real4 ptr [myFloat]
+	
 	ret
 SomeFunction endp
-end	
+end
